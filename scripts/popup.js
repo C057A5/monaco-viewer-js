df8a4d73163e97238babf6c597be3c7f7387e0a8
Costas <C057A5@users.noreply.github.com>
2022-08-27 16:01:30 +0300

    Add supported themes datalist

--- scripts/popup.js
+++ scripts/popup.js
@@ -12,7 +12,7 @@ function loadSettings() {
 	document.getElementById("save").addEventListener("click", saveSettings, false);
 	chrome.storage.sync.get('settings', function (data) {
 		var settings = data && data.settings ? data.settings : {};
-		if (!settings.theme) settings.theme = 'vs-light';
+		if (!settings.theme) settings.theme = '';
 		if (!settings.fontFamily) settings.fontFamily = 'Consolas';
 		if (!settings.fontSize) settings.fontSize = 12;
 		if (!settings.fontWeight) settings.fontWeight = '400';
